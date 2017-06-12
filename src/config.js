db9e4344769b0314149a3cdecd2595ec5de17069
parthipansowmi <parthipansowmi@gmail.com>
2017-06-12 20:50:59 +0530

    changed the port number

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const port = process.env.PORT || 3000;
 export const host = process.env.WEBSITE_HOSTNAME || `localhost:${port}`;
 
-export const apiport = process.env.PORT || 3002;
+export const apiport = process.env.PORT || 3006;
 export const apihost = process.env.WEBSITE_HOSTNAME || `localhost:${apiport}`;
 
 export const databaseUrl = process.env.DATABASE_URL || 'sqlite:database.sqlite';
